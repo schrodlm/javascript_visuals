bda9a913b5216b6cb1ee48d2ea53eb6c9d66ef4c
Matěj Schrödl <msamot@seznam.cz>
2022-09-15 22:46:47 +0200

    utilities&randomness

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,12 +1,18 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const degToRad = (degrees) => {
-  return degrees / 180 * Math.PI;
-};
+// const degToRad = (degrees) => {
+//   return degrees / 180 * Math.PI;
+// };
+
+// const randomRange = (min,max) =>{
+//   return Math.random() * (max-min) + min;
+// }
 
 
 const sketch = () => {
@@ -19,7 +25,7 @@ const sketch = () => {
     const cx = width * 0.5;
     const cy = height * 0.5;
 
-    const w = width * 0.03;
+    const w = width * 0.01;
     const h = height * 0.1;
     let x,y;
 
@@ -28,7 +34,7 @@ const sketch = () => {
 
     for(let i = 0; i < num; i++)
     {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = cx + radius * Math.sin(angle);
@@ -37,17 +43,22 @@ const sketch = () => {
       context.save();
       context.translate(x,y);
       context.rotate(-angle);
+      context.scale(random.range(0.1,2),random.range(0.2,0.5));
       
-      if(i % 2 == 0)
-      {
-      context.rect(-w * 0.5, -h * 0.5, w, h + 49);
-      }
-      else
-      {
-        context.rect(-w * 0.5, -h * 0.5, w, h);
-      }
 
+      context.beginPath();
+      context.rect(-w * 0.5, random.range(0,-h * 0.5), w, h);
       context.fill(); 
+      context.restore();
+
+      context.save();
+      context.translate(cx,cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5,20);
+      context.beginPath();
+      context.arc(0,0,radius * random.range(0.7, 1.3),slice * random.range(1,-8),slice * random.range(1,5));
+      context.stroke();
       context.restore();
     }
     // context.beginPath();
