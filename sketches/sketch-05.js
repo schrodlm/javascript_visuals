d8889cd08c108612f815c780d95765e0ba7b8ad3
Matěj Schrödl <msamot@seznam.cz>
2022-09-23 21:22:36 +0200

    mapping onto canvas

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -6,42 +6,93 @@ const settings = {
 
 let manager;
 
-let text = 'Hello World';
-let fontSize = '120';
+let text = 'h';
+let fontSize = '1200';
 let fontFamily = 'Courier New';
 
-const sketch = () => {
+//vytvoření vlastního canvasu
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+
+  //CREATING GRID
+  //one cell of specified size
+  const cell = 20;
+  const cols = Math.floor(width/cell);
+  const rows = Math.floor(height/cell);
+  const numCells = cols * rows;
+
+  // proportions for my canvas
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
+  fontSize = cols;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
-    //context.font = fontSize + 'px' + fontFamily;
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = 'top';
+    typeContext.fillStyle = 'orange';
+    //typeContext.font = fontSize + 'px' + fontFamily;
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = 'top';
     
-    //context.textAlign = 'center'; -- default value = 'left';
-
-    const metrics = context.measureText(text);
+    //tady "měřím" text s kterým pracuju a podle toho ho vykresluju aby byl presně ve středu
+    // kazdy font ma totiz trosku jine postaveni pismen atd.
+    const metrics = typeContext.measureText(text);
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-    const x = (width - mw) * 0.5 - mx;
-    const y = (height - mh) * 0.5 - my;
-    console.log(metrics);
+    const tx = (cols - mw) * 0.5 - mx;
+    const ty = (rows - mh) * 0.5 - my;
 
-    context.save();
-    context.translate(x, y);
+    typeContext.save();
+    typeContext.translate(tx, ty);
     
-    context.beginPath();
-    context.rect(mx,my,mw,mh);
-    context.stroke();
+    typeContext.beginPath();
+    typeContext.rect(mx,my,mw,mh);
+    typeContext.stroke();
     
-    context.fillText(text,0,0);
-    context.restore();
+    typeContext.fillText(text,0,0);
+    typeContext.restore();
 
+    //extrakce informaci z mnou vytvořeného canvasu
+    const typeData = typeContext.getImageData(0,0,cols,rows).data;
+    console.log(typeData);
+    
+
+    // tady barvím jednotlivé buňky v mnou vytvořeném gridu 
+    // podle toho co jsem extractnul z mnou vytvořeného canvasu
+    for(let i = 0; i < numCells; i++)
+    {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      const r = typeData[i * 4];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      context.fillStyle = `rgb(${r},${g},${b})`;
+
+
+      context.save();
+      context.translate(x,y);
+      context.translate(cell * 0.5, row * 0.5);
+
+      context.beginPath();
+      context.arc(0,0,cell / 2,0, Math.PI * 2);
+      context.fill();
+      context.restore();
+    }
+
+    context.drawImage(typeCanvas,0,0);
   };
 };
 
